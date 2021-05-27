178507a3f0af9ebde20886ed53c4cf9bdfe4efde
Henrique Torres <henriquetfw@gmail.com>
2021-05-26 22:17:57 -0300

    Refactor: move routes from shared into the modules directory

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
-import { authenticateRoutes } from './authenticate.routes';
-import { carsRoutes } from './cars.routes';
-import { categoriesRoutes } from './categories.routes';
-import { specificationRoutes } from './specification.routes';
-import { usersRoutes } from './users.routes';
+import { authenticateRoutes } from '@modules/accounts/infra/http/routes/authenticate.routes';
+import { usersRoutes } from '@modules/accounts/infra/http/routes/users.routes';
+import { carsRoutes } from '@modules/cars/infra/http/routes/cars.routes';
+import { categoriesRoutes } from '@modules/cars/infra/http/routes/categories.routes';
+import { specificationRoutes } from '@modules/cars/infra/http/routes/specification.routes';
 
 export const router = Router();
 
