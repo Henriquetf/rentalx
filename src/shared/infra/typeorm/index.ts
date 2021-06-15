964cc85ed903c10a269e5961c3ab13f20d7610c7
Henrique Torres <henriquetfw@gmail.com>
2021-06-14 22:37:58 -0300

    Remove hardcoded database host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export async function makeDatabaseConnection(host = 'database'): Promise<Connection> {
+export async function makeDatabaseConnection(): Promise<Connection> {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
       database: process.env.NODE_ENV === 'test' ? 'rentalx_test' : defaultOptions.database,
     }),
   );
